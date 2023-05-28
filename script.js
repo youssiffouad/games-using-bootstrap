1f453f5228a520ea34c2700458bffe062a0f233d
unknown <youssif1999fouad@gmail.com>
2023-05-28 23:14:36 +0300

    dynamic generation of cards v-1

--- script.js
+++ script.js
@@ -1,53 +1,18 @@
+generatediv();
+rdsubmit();
+
 //----------dynamic generation of one row inside container-----------------
 let mycontainer = document.body.querySelector(
   ".container-sm.d-flex.flex-column.justify-content-center"
 );
 
-function generaterow(x, y) {
-  for (let i = 0; i < x; i++) {
-    console.log(`row${i}`);
-    let myrow = document.createElement("div");
-    myrow.classList.add(
-      "row",
-      "align-content-center",
-      "flex-fill",
-      "justify-content-center"
-    );
-    mycontainer.appendChild(myrow);
-    for (let j = 0; j < y; j++) {
-      appendcardtorow(myrow, y);
-      console.log(`card${j}`);
-    }
-  }
-}
-
-//-------------------dynamic generation of cards inside row----------------
-function appendcardtorow(r, no) {
-  let mycard = document.createElement("div");
-  mycard.classList.add("col-3", "m-1");
-  r.appendChild(mycard);
-  appendfrbk(mycard);
-}
-
-//-------caliing of generation fns----------
-generaterow(6, 3);
-
-//--------appending front and back faces to cards--------------
-function appendfrbk(card) {
-  let front = document.createElement("div");
-  front.classList.add("front", "setbgfront");
-  let back = document.createElement("div");
-  back.classList.add("back");
-  card.append(front, back);
-}
+let noofcards = decide();
+console.log(`no of cards=${noofcards}`);
 
 //select all cards
 let cards = document.querySelectorAll(".row .col-3 ");
-
-//-------------randomly distibute 4 imgs across back face------------------
-
 let noofimgs = 4;
-let noofcards = 18;
+
 //1- creating array of indesxes of 12 photos
 let imgs = [];
 let imgindex = 1;
@@ -59,6 +24,8 @@ for (let i = 0; i < noofcards; i += 2) {
   imgs[i + 1] = imgindex;
   imgindex++;
 }
+console.log(`no of cards=${noofcards}`);
+console.log(`no of imgs=${imgs.length}`);
 
 //2-randomly distributing indexes (shuffling)
 
@@ -96,6 +63,115 @@ for (let i = 0; i < noofcards; i++) {
     prev = curr;
   });
 }
+
+//-----------------------------------------------------------------\
+//                                                                 \
+//--------------------------function corner--------------------    \
+//                                                                 \
+//-----------------------------------------------------------------\
+
+function generaterow(x, y) {
+  for (let i = 0; i < x; i++) {
+    let myrow = document.createElement("div");
+    myrow.classList.add(
+      "row",
+      "align-content-center",
+      "flex-fill",
+      "justify-content-center"
+    );
+    mycontainer.appendChild(myrow);
+    for (let j = 0; j < y; j++) {
+      appendcardtorow(myrow, y);
+    }
+  }
+}
+
+//-------------------dynamic generation of cards inside row----------------
+function appendcardtorow(r, no) {
+  let mycard = document.createElement("div");
+  mycard.classList.add("col-3", "m-1");
+  r.appendChild(mycard);
+  appendfrbk(mycard);
+}
+
+//--------appending front and back faces to cards--------------
+function appendfrbk(card) {
+  let front = document.createElement("div");
+  front.classList.add("front", "setbgfront");
+  let back = document.createElement("div");
+  back.classList.add("back");
+  card.append(front, back);
+}
+
+//----generate div of choosing difficulty----------
+function generatediv() {
+  let choicediv = document.createElement("div");
+  choicediv.classList.add("choicediv");
+  console.log(choicediv.innerHTML);
+  let parent = document.querySelector(
+    ".container-sm.d-flex.flex-column.justify-content-center"
+  );
+  console.log(parent);
+  parent.appendChild(choicediv);
+  let myform = document.querySelector("form");
+  choicediv.appendChild(myform);
+  myform.classList.remove("d-none");
+}
+
+//-------------------sending user choice to js code------------------
+
+function rdsubmit() {
+  let rds = document.querySelectorAll('input[type="radio"]');
+  console.log(rds);
+
+  rds.forEach((ele) => {
+    ele.addEventListener("click", btnsubmit);
+  });
+}
+
+function btnsubmit() {
+  let btn = document.querySelector(".btn.btn-primary");
+  btn.addEventListener("click", sendreply);
+  console.log(btn);
+}
+
+function sendreply() {
+  let chosen = document.querySelector(
+    'input[ name="flexRadioDefault"]:checked'
+  );
+  console.log(chosen.value);
+  console.log("from send reply");
+}
+
+//-----------deciding on no of cards based on radio ip------------
+function getrdvalue() {
+  let rdvalue = document.querySelector('input[type="radio"]:checked').value;
+  return rdvalue;
+}
+
+//--------------we have to get value of radio btn only after clicking-------------
+function afterclk() {}
+
+function decide() {
+  let noofrows,
+    noofcols = 3;
+  console.log(12);
+  console.log(getrdvalue());
+  if (getrdvalue() === 1) {
+    noofrows = 4;
+  } else if (getrdvalue() === 2) {
+    noofrows = 5;
+  } else {
+    noofrows = 6;
+  }
+  //-------caliing of generation fns----------
+
+  generaterow(noofrows, noofcols);
+  return noofrows * noofcols;
+}
+
+//-------------randomly distibute 4 imgs across back face------------------
+
 function flipcountercheck() {
   if (flipcounter % 2 == 0) {
     evencheck();
